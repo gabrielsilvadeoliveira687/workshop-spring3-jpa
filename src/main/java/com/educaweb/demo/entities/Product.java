acbdac04cdb54e4c3495e5681868a7bef3da1278
Gabriel Silva de Oliveira <gabrielsilvadeoliveira687@gmail.com>
2023-08-14 17:21:52 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/educaweb/demo/entities/Product.java
+++ src/main/java/com/educaweb/demo/entities/Product.java
@@ -1,3 +1,4 @@
+
 package com.educaweb.demo.entities;
 
 import java.io.Serializable;
@@ -5,12 +6,17 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name ="tb_product")
@@ -23,11 +29,21 @@ public class Product implements Serializable {
 	private String name;
 	private	String description;
 	private Double price;
-	private String imgUrl;
+	private String imgUrl;	
 	
-	@Transient
+	@ManyToMany
+	@JoinTable(name = "tb_product_category",
+    joinColumns=@JoinColumn(name="product_id"),
+    inverseJoinColumns=@JoinColumn(name="category_id"))
 	private Set<Category> categories= new HashSet<>();
 
+	
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
+	
+	
+	
 	public Product() {}
 	
 
@@ -69,15 +85,30 @@ public class Product implements Serializable {
 		this.imgUrl = imgUrl;
 	}
 	
-	
 	public Set<Category> getCategories() {
 		return categories;
 	}
+	
+	
+	@JsonIgnore
+	public Set<Order> getOrders(){
+		Set<Order>set=new HashSet<>(); 
+		for (OrderItem x : items ) {
+			set.add(x.getOrder( ));
+		}
+		
+		return set;
+		
+	}
 
-
+	
+	
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime =31;
+		int result =1;
+		result =prime *result +((id==null)?0:id.hashCode()); 
+		return result;
 	}
 
 	@Override
