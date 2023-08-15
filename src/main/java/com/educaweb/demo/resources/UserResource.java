1f00b21650df736b89dfda9bfd0ec7f5908b474a
Gabriel Silva de Oliveira <gabrielsilvadeoliveira687@gmail.com>
2023-08-15 17:37:39 -0300

    User insert

--- src/main/java/com/educaweb/demo/resources/UserResource.java
+++ src/main/java/com/educaweb/demo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.educaweb.demo.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educaweb.demo.entities.User;
 import com.educaweb.demo.services.UserService;
@@ -34,8 +36,9 @@ public class UserResource {
 	
 	@PostMapping
 	public ResponseEntity<User> insert(@RequestBody  User obj){
-		obj = service.insert(obj);								
-		return ResponseEntity.ok().body(obj);
+		obj = service.insert(obj);	
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);	
 			
 	}
 	}
