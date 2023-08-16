ea67c649d606b345be432b0e4572393b7afb616d
Gabriel Silva de Oliveira <gabrielsilvadeoliveira687@gmail.com>
2023-08-16 17:07:56 -0300

    Exception handling - findById

--- src/main/java/com/educaweb/demo/services/UserService.java
+++ src/main/java/com/educaweb/demo/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educaweb.demo.entities.User;
 import com.educaweb.demo.repositories.UserRepository;
+import com.educaweb.demo.services.exceptions.DatabaseException;
 import com.educaweb.demo.services.exceptions.ResourceNotFoundException;
 
 
@@ -33,7 +36,15 @@ public class UserService {
 		}
 		
 		public void delete(Long id) {
-			repository.deleteById(id);
+			try {
+				
+				repository.deleteById(id);
+			}catch(EmptyResultDataAccessException e ) {
+				throw new  ResourceNotFoundException(id);
+			}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException (e.getMessage());
+
+		}
 		}
 		public User update(Long id, User obj) {
 			User entity = repository.getReferenceById(id);
